a348ce32f0fbd11ba3f88a2c2df2a17570f4ef7e
Russ Olsen <russ@cognitect.com>
2022-10-29 14:09:21 -0400

    Add support for else: and elif: to EPy.

--- src/qrn/epy.py
+++ src/qrn/epy.py
@@ -10,6 +10,8 @@ SplitRE = r'(?=<%)|=%>\n?|!%>\n?|%>'
 
 CODE_RE = re.compile(r'^<%')
 END_RE = re.compile(r'^<%! *end *$')
+ELSE_RE = re.compile(r'^<%! *else: *$')
+ELIF_RE = re.compile(r'^<%! *elif .*:')
 EX_RE = re.compile(r'^<%!')
 INC_RE = re.compile(r'^<%=')
 
@@ -20,6 +22,11 @@ def _compile_template(fragments):
             generator.text(frag)
         elif END_RE.search(frag):
             generator.dedent()
+        elif ELSE_RE.search(frag) or ELIF_RE.search(frag):
+            generator.dedent()
+            code = frag[3:].strip()
+            generator.code(code)
+            generator.indent()
         elif EX_RE.search(frag):
             code = frag[3:].strip()
             generator.code(code)
@@ -29,21 +36,19 @@ def _compile_template(fragments):
             expr = frag[3:]
             generator.expr(expr)
     if generator.depth != 0:
-        raise Exception("Unclosed blocks in epy template!")
+        print("Generator depth:", generator.depth)
+        #raise Exception("Unclosed blocks in epy template!")
     return generator.output
 
-def template_from_text(ttext, desc='template'):
+def source_code_for(ttext):
     fragments = re.split(SplitRE, ttext)
     output = _compile_template(fragments)
+    return output
+
+def template_from_text(ttext, desc='template'):
+    #fragments = re.split(SplitRE, ttext)
+    #output = _compile_template(fragments)
+    output = source_code_for(ttext)
     code = utils.compile_string(output, desc)
     return code
-
-def template_f_from_text(ttext, desc='template'):
-    code = template_from_text(ttext, desc)
-    def render(globs={}, locs={}):
-        return utils.exec_prog_output(code, globs, locs)  
-    return render
-
-def template_f_from_path(path):
-    text = utils.read_file(path)
-    return template_f_from_text(text, path)
+ 
